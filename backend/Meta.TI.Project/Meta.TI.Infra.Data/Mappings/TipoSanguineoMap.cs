cac51e4b3e4971911dcbba9498b19df9c543a296
Igor Walace Cuevas <igor.w.cuevas@gmail.com>
2020-05-24 21:40:21 -0300

    Seed Tipo Sanguineo

--- backend/Meta.TI.Project/Meta.TI.Infra.Data/Mappings/TipoSanguineoMap.cs
+++ backend/Meta.TI.Project/Meta.TI.Infra.Data/Mappings/TipoSanguineoMap.cs
@@ -1,4 +1,4 @@
-﻿using Meta.TI.Domain.Models;
+using Meta.TI.Domain.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
@@ -19,6 +19,49 @@ namespace Meta.TI.Infra.Data.Mappings
                 .HasColumnType("varchar(50)")
                 .HasColumnName("Nome")
                 .IsRequired();
+
+            builder.HasData(
+                new TipoSanguineo
+                {
+                    Id = 1,
+                    Nome = "A+"
+                },
+                new TipoSanguineo
+                {
+                    Id = 2,
+                    Nome = "A-"
+                },
+                new TipoSanguineo
+                {
+                    Id = 3,
+                    Nome = "AB+"
+                },
+                new TipoSanguineo
+                {
+                    Id = 4,
+                    Nome = "AB-"
+                },
+                new TipoSanguineo
+                {
+                    Id = 5,
+                    Nome = "B+"
+                },
+                new TipoSanguineo
+                {
+                    Id = 6,
+                    Nome = "B-"
+                },
+                new TipoSanguineo
+                {
+                    Id = 7,
+                    Nome = "O+"
+                },
+                new TipoSanguineo
+                {
+                    Id = 8,
+                    Nome = "O-"
+                }
+                );
         }
     }
 }
---
+++ backend/Meta.TI.Project/Meta.TI.Infra.Data/Migrations/20200525003457_Seed-Tipo-Sanguineo.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Meta.TI.Infra.Data.Migrations
+{
+    public partial class SeedTipoSanguineo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IdTipoLogin",
+                table: "Usuario");
+
+            migrationBuilder.InsertData(
+                table: "TipoSanguineo",
+                columns: new[] { "Id", "Nome" },
+                values: new object[,]
+                {
+                    { 1, "A+" },
+                    { 2, "A-" },
+                    { 3, "AB+" },
+                    { 4, "AB-" },
+                    { 5, "B+" },
+                    { 6, "B-" },
+                    { 7, "O+" },
+                    { 8, "O-" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "TipoSanguineo",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "TipoSanguineo",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "TipoSanguineo",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "TipoSanguineo",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "TipoSanguineo",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "TipoSanguineo",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "TipoSanguineo",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "TipoSanguineo",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.AddColumn<int>(
+                name: "IdTipoLogin",
+                table: "Usuario",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
