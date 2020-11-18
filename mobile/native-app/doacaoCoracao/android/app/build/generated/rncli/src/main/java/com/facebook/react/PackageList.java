95f7672ddae2c79b8d08bff65e477628c6f20f52
Igor Cuevas <igor.w.cuevas@gmail.com>
2020-11-18 10:07:11 -0300

    commit da salvação

--- mobile/native-app/doacaoCoracao/android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
+++ mobile/native-app/doacaoCoracao/android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -13,6 +13,8 @@ import java.util.ArrayList;
 
 // @react-native-community/async-storage
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+// @react-native-community/datetimepicker
+import com.reactcommunity.rndatetimepicker.RNDateTimePickerPackage;
 // @react-native-community/geolocation
 import com.reactnativecommunity.geolocation.GeolocationPackage;
 // @react-native-community/masked-view
@@ -79,6 +81,7 @@ public class PackageList {
     return new ArrayList<>(Arrays.<ReactPackage>asList(
       new MainReactPackage(mConfig),
       new AsyncStoragePackage(),
+      new RNDateTimePickerPackage(),
       new GeolocationPackage(),
       new RNCMaskedViewPackage(),
       new RNFusedLocationPackage(),
